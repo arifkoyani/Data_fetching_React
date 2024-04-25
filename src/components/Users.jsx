3971dbc75b78f38759b6cf4ec755c0c2f4cd70c7
arifkoyani <arifkoyani@gmail.com>
2024-04-25 05:39:55 -0700

    updated code

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -3,30 +3,33 @@ import React, { useState, useEffect } from "react";
 let Users = () => {
   let [data, setData] = useState([]);
   let [page, setPage] = useState(1);
-  let perPage=10;
 
+
+  
 
   useEffect(() => {
     const fetchData = async () => {
-      
+      let perPage=15;
       try {
         const response = await fetch(`https://api.github.com/search/users?q=followers:>0&sort=followers&order=desc&page=${page}&per_page=${perPage}`);
         const jsonData = await response.json();
-        console.log(jsonData)
-        setData((prevData) => [...prevData, ...jsonData.items]);
-      } catch (error) {
-        console.error("Error fetching data:", error);
 
+        setData((prevData) => [...prevData, ...jsonData.items]);
+
+      } 
+      
+      catch (error) {
+        console.error("Error fetching data:", error);
       }
     };
 
+
     fetchData();
-
-
   }, [page]);
 
 
   useEffect(() => {
+
     window.addEventListener("scroll", () => {
       const { scrollTop, clientHeight, scrollHeight } =
         document.documentElement;
@@ -37,6 +40,8 @@ let Users = () => {
       }
     });
   }, []);
+
+
 
   return (
     <>   
